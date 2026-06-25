e8b2c1f25673ad995dca9664eee9e18129060be3
Rafael Henrique <100523456+devrafaelsoares@users.noreply.github.com>
2026-06-24 21:39:07 -0300

    style(two-pointers): format two pointers solution
    Standardized indentation, pointer placement, and spacing for clarity.

--- two_pointers.cc
+++ two_pointers.cc
@@ -1,41 +1,36 @@
-#include <vector>
 #include <iostream>
+#include <vector>
 using namespace std;
 
-class Solution 
-{
-    public:
-    int removeDuplicates(vector<int>& nums) 
-    {
-        int left = 0;
-        int right = nums.size() - 1;
-        int temp;
+class Solution {
+public:
+  int removeDuplicates(vector<int> &nums) {
+    int left = 0;
+    int right = nums.size() - 1;
+    int temp;
 
-        while (left < right) {
+    while (left < right) {
 
-            temp = nums[left];
+      temp = nums[left];
 
-            left++;
-            right--;
-        }
-
-        return 0;
+      left++;
+      right--;
     }
-};
-
-
-
-int main()
-{
-
-    Solution* solution = new Solution();
-    vector<int> nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-
-    int expected = solution->removeDuplicates(nums);
-
-    cout << "Resultado: " << expected << endl;
-
-    delete solution;
 
     return 0;
+  }
+};
+
+int main() {
+
+  Solution *solution = new Solution();
+  vector<int> nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+
+  int expected = solution->removeDuplicates(nums);
+
+  cout << "Resultado: " << expected << endl;
+
+  delete solution;
+
+  return 0;
 }
